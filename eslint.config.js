263a1f01221f00197299861573402e9808b69186
Douglas Galdino <dougg0k@gmail.com>
2025-07-26 12:05:52 -0300

    fixed some paths

--- eslint.config.js
+++ eslint.config.js
@@ -12,7 +12,7 @@ export default tseslint.config(
     "apps/test-app/ios/**",
     "packages/host/hermes/**",
     "packages/node-addon-examples/examples/**",
-    "packages/ferric-example/ferric_example.d.ts",
+    "packages/ferric-example/dist/ferric_example.d.ts",
   ]),
   eslint.configs.recommended,
   tseslint.configs.recommended,
