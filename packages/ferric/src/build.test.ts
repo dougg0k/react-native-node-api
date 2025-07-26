789e2b8a14aff9710650dcb744adbe6664d77cdd
Douglas Galdino <dougg0k@gmail.com>
2025-07-26 19:01:26 -0300

    removed test files

--- packages/ferric/src/build.test.ts
+++
@@ -1,9 +0,0 @@
-import assert from "node:assert/strict";
-import { describe, it } from "node:test";
-
-describe("build", () => {
-  it("", () => {
-    // Needs init, to generate project and then build
-    assert.equal(1, 1);
-  });
-});
